82c684ee7ae932bece2c8c841a85dfe9695f8b2d
Taylor Swanson <kaleidoflowers@gmail.com>
2016-01-06 19:52:48 -0700

    Added some simple tests

--- test/index.js
+++ test/index.js
@@ -5,7 +5,28 @@ var expect = require("chai").expect;
 var StickyServer = rewire("../lib/");
 
 describe("StickyServer", function() {
+    var sticky;
 
+    beforeEach(function() {
+
+    });
+    it("should allow missing config object", function() {
+        sticky = new StickyServer();
+
+        expect(sticky).to.have.property("config");
+    });
+    it("should apply default config values when object is undefined", function() {
+        sticky = new StickyServer();
+
+        expect(sticky.config).to.have.property("respawnWorkers").and.equal("always");
+        expect(sticky.config).to.have.property("maxWorkers").and.equal(0);
+    });
+    it("should apply only provided config values", function() {
+        sticky = new StickyServer({ maxWorkers: 4 });
+
+        expect(sticky.config).to.have.property("respawnWorkers").and.equal("always");
+        expect(sticky.config).to.have.property("maxWorkers").and.equal(4);
+    });
 });
 
 describe("StickyServer Master", function() {
